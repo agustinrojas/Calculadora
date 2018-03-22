958be8f224e94dffbb737c4845ff4860b24ab45f
ATMIRA\agustin.rojas <agustin.rojas@atmira.com>
2018-03-22 18:47:09 +0100

    Subimos el proyecto

---
+++ PooVuelingTests/CalculadoraIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVueling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling.Tests
+{
+    [TestClass()]
+    public class CalculadoraIntegrationTests
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplicacionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            //.net su framework tiene una case que se llama assrt con un metodo estatco IsTrue, que comprueba que la condicion que le pasa sea verdadera
+            Assert.IsTrue(iCalculadora.Suma(2, 2) == 4);
+            //Assert.Fail();
+        }
+    }
+}
---
+++ PooVuelingXunitTests/CalculadoraIntegrationXunitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using PooVueling;
+
+
+
+
+namespace PooVuelingXunitTests
+{
+    public class CalculadoraIntegrationXunitTests
+    {
+        ICalculadora iCalculadora = new Calculadora();
+
+        [Fact]
+        public void SumaTest()
+        {
+            Assert.Equal(10, iCalculadora.Suma(5, 5));
+        }
+
+        [Fact]
+        public void RestaTest()
+        {
+            Assert.Equal(1, iCalculadora.Resta(4, 3));
+        }
+
+        [Fact]
+        public void DivisionTest()
+        {
+            Assert.False(6 == iCalculadora.Division(24, 3));
+        }
+
+        [Fact]
+        public void MultiplicacionTest()
+        {
+            Assert.True(4 == iCalculadora.Multiplicacion(2, 2));
+        }
+    }
+}
